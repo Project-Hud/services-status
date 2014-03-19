e3f506cc4f8a6ca9762ccdcae529f432dbd47d4a
James Mortemore <james.mortemore@clock.co.uk>
2014-03-19 16:16:21 +0000

    Updated to use hud-widget module

--- app.js
+++ app.js
@@ -1,35 +1,9 @@
+var request = require('request')
+  , Widget = new require('hud-widget')
+  , widget = new Widget()
+  , services = JSON.parse(process.env.SERVICES)
 
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , http = require('http')
-  , path = require('path')
-  , request = require('request')
-  , app = express()
-  , cache = {}
-
-// all environments
-app.set('port', process.env.PORT || 3000)
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
-app.use(express.favicon())
-app.use(express.logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded())
-app.use(express.methodOverride())
-app.use(app.router)
-app.use(express.static(path.join(__dirname, 'public')))
-
-var services = JSON.parse(process.env.SERVICES)
-
-// development only
-if ('development' === app.get('env')) {
-  app.use(express.errorHandler())
-}
-
-app.get('/', function (req, res) {
+widget.get('/', function (req, res) {
   res.render('index',
     { services: process.env.SERVICES
     , serviceCount: Object.keys(services).length
@@ -37,19 +11,10 @@ app.get('/', function (req, res) {
     })
 })
 
-app.get('/status/:serviceName', function (req, res) {
+widget.get('/status/:serviceName', widget.cache(5000), function (req, res) {
   var serviceName = req.params.serviceName
 
   if (!services[serviceName]) return res.send(404)
-
-  var cached = cache[serviceName]
-    , now = new Date().getTime()
-
-  if (cached) {
-    var difference = now - cached.lastChecked
-
-    if (difference < 5000) return res.send(cached.resCode)
-  }
 
   request(
     { url: services[serviceName]
@@ -58,16 +23,7 @@ app.get('/status/:serviceName', function (req, res) {
       var resCode = 200
       if (error || response.statusCode !== 200) resCode = 500
 
-      cache[serviceName] =
-        { lastChecked: new Date().getTime()
-        , resCode: resCode
-        }
-
       return res.send(resCode)
     })
 
-})
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'))
 })
