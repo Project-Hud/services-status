4b3a7ae9920f50cd72074e52c3e404997ae98189
Nick Price <nick.price@clock.co.uk>
2014-04-01 10:47:53 +0100

    Updated service loaders & added text

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
   var $moduleBody = $('.module__body')
     , statuses = Object.keys(JSON.parse($moduleBody.attr('data-services')))
     , successClass = 'success'
-    , failureClass = 'danger'
     , timeout = $moduleBody.attr('data-timer')
 
   $.each(statuses, checkStatus)
@@ -11,31 +10,19 @@ $(document).ready(function () {
     $.each(statuses, checkStatus)
   }, timeout)
 
-  function toggleLoading($serviceEl, hide) {
-    var $status = $serviceEl.find('.js-status')
-      , $loading = $serviceEl.find('.js-loading')
-
-    if (hide) {
-      $loading.hide()
-      $status.show()
-    } else {
-      $status.hide()
-      $loading.show()
-    }
-  }
-
   function checkStatus(i, status) {
     var $serviceEl = $('.js-service[data-service=' + status + ']')
-    toggleLoading($serviceEl, false)
+      , $loading = $serviceEl.find('.js-loading').show()
+
     $.ajax(
       { url: '/status/' + status
       , success: function () {
-          toggleLoading($serviceEl, true)
-          $serviceEl.removeClass(failureClass).addClass(successClass)
+          $loading.hide()
+          $serviceEl.addClass(successClass)
         }
       , error: function () {
-          toggleLoading($serviceEl, true)
-          $serviceEl.removeClass(successClass).addClass(failureClass)
+          $loading.hide()
+          $serviceEl.removeClass(successClass)
         }
       })
   }
